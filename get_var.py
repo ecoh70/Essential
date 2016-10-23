9255031047b304fe894447f6534b2ba5c659317d
Connor E. Haight <connor.haight@gmail.com>
2016-10-23 15:56:09 -0500

    Update get_var.py

--- get_var.py
+++ get_var.py
@@ -4,8 +4,13 @@ variables = {}
 
 def getVar(key):
     if key[0] == '%':
+        if key[1:] not in variables:
+            return None
         return variables[key[1:]]
     elif key[-1] in ('+', '-', '/', '*'):
         return parse_expr(key)
     else:
         return key
+
+def set_var(key, val):
+    variables[key] = get_var(val)
