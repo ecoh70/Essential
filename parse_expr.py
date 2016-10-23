4f1e0c97100161b210d63b065ead35f602b62d9a
Connor E. Haight <connor.haight@gmail.com>
2016-10-23 15:47:06 -0500

    Create parse_expr.py

---
+++ parse_expr.py
@@ -0,0 +1,19 @@
+import get_var
+
+def parse_expr(expr):
+    stack = []
+    for term in expr:
+        if not term in ('+', '-', '*', '/'):
+            stack.append(get_var(term))
+        elif term in ('+', '-', '/', '*'):
+            operand = stack.pop()
+            operand2 = stack.pop()
+            if term == '+':
+                stack.append(operand1 + operand2)
+            elif term == '-':
+                stack.append(operand1 - operand2)
+            elif term == '*':
+                stack.append(operand1 * operand2)
+            elif term == '/':
+                stack.append(operand1 / operand2)
+    return stack[0]
