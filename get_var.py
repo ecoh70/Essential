dffbf1211afa4891087b81c63267260f170ccd39
Connor E. Haight <connor.haight@gmail.com>
2016-10-23 19:38:57 -0500

    Update get_var.py

--- get_var.py
+++ get_var.py
@@ -1,15 +1,19 @@
-import parse_expr
+import parse_expr import parse_expr
 
-def getVar(variables, key):
-    if key[0] == '%':
-        if key[1:] not in variables:
-            return None
-        return variables[key[1:]]
-    elif key[-1] in ('+', '-', '/', '*'):
-        return parse_expr(key)
-    else:
-        return key
+class var_hand(object):
+    def __init__(self):
+        variables = {}
 
-def set_var(variables, key, val):
-    variables[key] = get_var(val)
-    return get_var(variables, key)
+    def getVar(self, key):
+        if key[0] == '%':
+            if key[1:] not in self.variables:
+                return None
+            return self.variables[key[1:]]
+        elif key[-1] in ('+', '-', '/', '*'):
+            return parse_expr(key)
+        else:
+            return key
+
+    def set_var(self, key, val):
+        self.variables[key] = get_var(val)
+        return get_var(key)
