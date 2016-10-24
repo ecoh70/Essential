d488c37406ce8b340365dbec272daa938a2b7a1b
Connor E. Haight <connor.haight@gmail.com>
2016-10-23 19:49:38 -0500

    Update and rename loop.py to test.py

---
+++ test.py
@@ -0,0 +1,30 @@
+def test(var, op, val):
+    if op == '==':
+        if get_var(var) == get_var(val):
+            return True
+        return False
+    
+    elif op == '!=':
+        if get_var(var) != get_var(val):
+            return True
+        return False
+    
+    elif op == '>':
+        if get_var(var) > get_var(val):
+            return True
+        return False
+    
+    elif op == '<':
+        if get_var(var) < get_var(val):
+            return True
+        return False
+    
+    elif op == '>=':
+        if get_var(var) >= get_var(val):
+            return True
+        return False
+    
+    elif op == '<=':
+        if get_var(var) <= get_var(val):
+            return True
+        return False
